2375d7da68b68ab69a7cd40ba16362ec9c0c7dc3
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-03-18 21:40:30 -0400

    Add city name and increase font size

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const stats = document.querySelector(".stats");
 const button = document.querySelector("button");
 const input = document.querySelector("input");
+const cityDiv = document.querySelector(".city-name");
 
 const key = "63a29a8fa8eb1f1f14a6fc6f868cc076";
 
@@ -18,9 +19,11 @@ button.addEventListener("click", (e) => {
       console.log(data);
       let temp_C = Math.round(data.main.temp - 273, 3);
       let temp_F = Math.round((temp_C * 9) / 5 + 32);
+
+      cityDiv.textContent = city;
       stats.textContent = `Current temp in ${city} is ${temp_C} C or ${temp_F} F. Wind is about ${data.wind.speed} m/s. Humidity is about ${data.main.humidity}%. Cloud cover around ${data.clouds.all}%.`;
     } catch {
-      alert("Pass a Value");
+      alert("Please pass a legitimate value");
     }
   }
 
